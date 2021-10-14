814215b914557ecd2743d82b3545a999ff9a67b8
bhavin121 <bhavin7014316242@gmail.com>
2021-10-14 11:40:10 +0530

    First Commit

--- app/src/main/java/com/bhavin/market/HomeFragment.java
+++ app/src/main/java/com/bhavin/market/HomeFragment.java
@@ -12,7 +12,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.bhavin.market.adapters.HomeAdapter;
+import com.bhavin.market.databinding.AddressDialogBinding;
 import com.bhavin.market.databinding.FragmentHomeBinding;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -23,6 +25,7 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private BottomSheetDialog addressDialog;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,9 +42,29 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view , @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState){
         super.onViewCreated(view , savedInstanceState);
 
+        buildAddressDialog();
+
         List<String> items = Arrays.asList("Fist","Second","Third", "Fourth");
         HomeAdapter adapter = new HomeAdapter(items);
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         binding.recyclerView.setAdapter(adapter);
+
+        binding.addressButton.setOnClickListener(view1 -> {
+            addressDialog.show();
+        });
+    }
+
+    private void buildAddressDialog(){
+        AddressDialogBinding binding = AddressDialogBinding.inflate(LayoutInflater.from(requireContext()));
+        addressDialog = new BottomSheetDialog(requireContext());
+        addressDialog.setContentView(binding.getRoot());
+
+        binding.addNewAddress.setOnClickListener(view -> {
+
+        });
+
+        binding.saveChanges.setOnClickListener(view -> {
+
+        });
     }
 }
---
+++ app/src/main/java/com/bhavin/market/ProfileFragment.java
@@ -0,0 +1,28 @@
+package com.bhavin.market;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bhavin.market.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
+
+    public ProfileFragment( ){
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater , ViewGroup container ,
+                             Bundle savedInstanceState){
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
